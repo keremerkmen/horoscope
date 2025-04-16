15710fd531f08ba13e069c8f137a37eb3d4c95a3
Kerem Erkmen <kerem_erkmen@uri.edu>
2025-04-16 11:59:36 -0400

    ..
    ..

--- includes/database-connection.php
+++ includes/database-connection.php
@@ -1,13 +1,13 @@
 <?php
 
 $type     = 'mysql';                             // Type of database
-$server   = 'yourIPAddress';                    // Server the database is on
-$db       = 'yourcPanelUsername_horoscope';    // Name of the database
+$server   = '192.185.2.183';                    // Server the database is on
+$db       = 'keremerk_horoscope';    // Name of the database
 $port     = '3306';                           // Port is usually 3306 in Hostgator
 $charset  = 'utf8mb4';                       // UTF-8 encoding using 4 bytes of data per char
 
-$username = 'yourcPanelUsername_user';     // Enter YOUR cPanel username and user here
-$password = 'yourUserPassword';           // Enter YOUR user password here                                     
+$username = 'keremerk_lab11Horoscope';     // Enter YOUR cPanel username and user here
+$password = 'Fenerb1907';           // Enter YOUR user password here                                     
 
 
 
